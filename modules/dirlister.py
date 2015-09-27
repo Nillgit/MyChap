25f5e1a6e704339862ad8a914f66dffdb82b931e
Nillgit <anilpriyanka@hotmail.com>
2015-09-27 13:35:50 +0530

    Adding new modules

--- modules/dirlister.py
+++ modules/dirlister.py
@@ -3,4 +3,4 @@ import os
 def run(**args):
     print "[*] In dirlister module."
     files = os.listdir(".")
-    return str(files)
+    return "Completed dirlistener"
--- modules/environment.py
+++ modules/environment.py
@@ -2,4 +2,5 @@ import os
 
 def run(**args):
     print "[*] In environment module."
-    return str(os.environ)
+    #return str(os.environ)
+    return "Completed environment.py"
